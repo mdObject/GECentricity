591afa3f4d69820c0f3bc2caf2c8d2a7fdb41ed4
Michael <mvasyliv@mail.ua>
2017-11-02 21:11:51 +0200

    clean up project, fixed bug with get mdObject

--- mdObject/examples/angular.js/scripts/DemoController.js
+++ mdObject/examples/angular.js/scripts/DemoController.js
@@ -1,17 +1,21 @@
-﻿/// <reference path="mdObject.js" /> 
+﻿/// <reference path="bundle.public.min.js" /> 
 
 var phonecatApp = angular.module('mdObjectDemo', []);
 
 phonecatApp.controller('DemoController', function ($scope, $parse) {
 
-    $scope.patient = $mdObject.patient;
-    $scope.version = $mdObject.version;
-    $scope.clinicalDocument = $mdObject.clinicalDocument;
-    $scope.LocationType = $mdObject.LocationType;
-    $scope.emr = $mdObject.emr;
-    $scope.emrUsers = $mdObject.users;
-    $scope.hwinstonUser = $mdObject.users.getUser('hwinston');
-    $scope.usersInCurrentLocation = $mdObject.users.getUsers();
-    $scope.currentUser = $mdObject.users.getUser();
-    $scope.mel = $mdObject.emr.mel;
+    if (mdObject != null) {
+        $scope.patient = mdObject.patient;
+        $scope.version = mdObject.version;
+        $scope.clinicalDocument = mdObject.clinicalDocument;
+        $scope.LocationType = mdObject.LocationType;
+        $scope.emr = mdObject.emr;
+        $scope.emrUsers = mdObject.users;
+        $scope.hwinstonUser = mdObject.users.getUser('hwinston');
+        $scope.usersInCurrentLocation = mdObject.users.getUsers();
+        $scope.currentUser = mdObject.users.getUser();
+        $scope.mel = mdObject.emr.mel;
+    } else {
+        alert('The "mdObject" is not defined.')
+    }
 });
