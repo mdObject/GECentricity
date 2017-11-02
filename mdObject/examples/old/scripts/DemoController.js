39e9f83d2bce28bf1497f5900a2d145cafbba588
Michael <mvasyliv@mail.ua>
2017-11-02 21:21:19 +0200

    added old example

---
+++ mdObject/examples/old/scripts/DemoController.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="mdObject.js" /> 
+
+var phonecatApp = angular.module('mdObjectDemo', []);
+
+phonecatApp.controller('DemoController', function ($scope, $parse) {
+
+    $scope.patient = $mdObject.patient;
+    $scope.version = $mdObject.version;
+    $scope.clinicalDocument = $mdObject.clinicalDocument;
+    $scope.LocationType = $mdObject.LocationType;
+    $scope.emr = $mdObject.emr;
+    $scope.emrUsers = $mdObject.users;
+    $scope.hwinstonUser = $mdObject.users.getUser('hwinston');
+    $scope.usersInCurrentLocation = $mdObject.users.getUsers();
+    $scope.currentUser = $mdObject.users.getUser();
+    $scope.mel = $mdObject.emr.mel;
+});
