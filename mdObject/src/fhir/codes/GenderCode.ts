a983fa1b796c2fd5f31c47acd04c0c131fa238f1
aaltotsky <alex@renetusa.com>
2020-08-08 21:59:36 -0700

    Added comments to GenderCode

--- mdObject/src/fhir/codes/GenderCode.ts
+++ mdObject/src/fhir/codes/GenderCode.ts
@@ -1,3 +1,7 @@
+/*
+ * Comply with https://www.hl7.org/fhir/valueset-administrative-gender.html
+ * The full fhir support expected in version 3 and above
+ */
 export enum GenderCode {
     male = 'male',
     female = 'female',
