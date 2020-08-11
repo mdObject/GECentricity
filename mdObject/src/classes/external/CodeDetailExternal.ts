3bc7d976342b2e30712cfc919f273c4c5b4a1103
aaltotsky <alex@renetusa.com>
2020-08-11 15:22:59 -0700

    Added ProblemsExternal and CodeDetailExternal

---
+++ mdObject/src/classes/external/CodeDetailExternal.ts
@@ -0,0 +1,12 @@
+export class CodeDetailExternal {
+    code: string;
+    codeType: number;
+    codeTypeSpecified: boolean;
+    longDescription: string;
+    masterDiagnosisId: number;
+    shortDescription: string;
+    specificity: number;
+    specificitySpecified: boolean;
+    status: number;
+    statusSpecified: boolean;
+}
