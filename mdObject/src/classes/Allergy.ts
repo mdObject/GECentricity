560ad54b9c6e90bc7f5b909d0e15a7f2d1f6e049
aaltotsky <alex@renetusa.com>
2020-08-15 21:30:42 -0700

    Added Alergy object

---
+++ mdObject/src/classes/Allergy.ts
@@ -0,0 +1,57 @@
+import { IAllergyData } from "../interfaces/interfaces";
+import { EmrMel } from "./EmrMel";
+import { ObjectState } from "../enums/objectState";
+
+export class Allergy implements IAllergyData {
+    state: ObjectState = ObjectState.None;
+
+    name: string;
+    onSetDate: string;
+    criticalIndicator: string;
+    classification: string;
+    description: string;
+    gpiCode: string;
+    severity: string;
+    stopDate: string;
+    allergyId: string;
+    reaction: string;
+
+    constructor(
+        public _mel: EmrMel
+    ) { }
+
+    save = (): void => {
+        switch (this.state) {
+            case ObjectState.Add: {
+                this._mel.melFunc('{MEL_REMOVE_ALLERGY("' + this.toAddString() + '")}');
+                this.state = ObjectState.None;
+                break;
+            }
+            case ObjectState.Update: {
+                this._mel.melFunc('{MEL_CHANGE_ALLERGY("' + this.toChangeString() + '")}');
+                this.state = ObjectState.None;
+                break;
+            }
+            case ObjectState.Remove: {
+                this._mel.melFunc('{MEL_REMOVE_ALLERGY("' + this.toRemoveString() + '")}');
+                this.state = ObjectState.None;
+                break;
+            }
+        }
+    }
+
+    private toChangeString = (): string => { return ''; }
+    private toRemoveString = (): string => { return ''; }
+
+    private toAddString = (): string => {
+        return this.name + '","' +
+            this.description + '","' +
+            this.onSetDate + '","' +
+            this.allergyId + '","' +
+            this.reaction + '","' +
+            this.gpiCode + '","' +
+            this.stopDate + '",,"' +
+            this.criticalIndicator + '","' +
+            this.classification;
+    }
+}
