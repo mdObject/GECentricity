cb80b464c160d66f9cc42ce9443aa6693b23b2d3
aaltotsky <alex@renetusa.com>
2020-08-23 00:21:15 -0700

    Added Allergy unit test

---
+++ mdObject/src/test/classes/Allergy.spec.ts
@@ -0,0 +1,41 @@
+import { Allergy } from "../../classes/classes";
+import { MockEmrMel } from "../mocks/mocks";
+import { AllergyClassification, AllergyCriticality, ObjectState } from "../../enums/enums";
+
+describe('Class: Allergy', () => {
+
+    let component: Allergy;
+    let mockEmrMel = new MockEmrMel();
+
+    beforeAll(() => {
+        component = new Allergy(mockEmrMel);
+    });
+
+    describe('save', () => {
+        beforeAll(() => {
+            component.classification = AllergyClassification.food;
+            component.criticalIndicator = AllergyCriticality.severe;
+            component.name = 'Seafood';
+            component.onSetDate = '01/02/2020';
+            component.description = 'Red skin';
+            spyOn(mockEmrMel, 'melFunc')
+                .and
+                .returnValue('');
+        });
+
+        it('Object state not set', () => {
+            component.state = ObjectState.None;
+            component.save();
+            expect(mockEmrMel.melFunc).not.toHaveBeenCalled();
+
+        });
+
+        it('Object state set to add', () => {
+            component.state = ObjectState.Add;
+            component.save();
+            expect(mockEmrMel.melFunc).toHaveBeenCalledWith('{MEL_ADD_ALLERGY("' + (component as any).toAddString() + '")}');
+        });
+
+    });
+
+})
