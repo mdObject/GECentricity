7894a7c500450471fd77100862b95162baac5db7
aaltotsky <alex@renetusa.com>
2021-01-03 11:57:33 -0800

    Replaced objectState reference

--- mdObjectAngularDemo/src/app/allergy/allergy.component.ts
+++ mdObjectAngularDemo/src/app/allergy/allergy.component.ts
@@ -1,7 +1,7 @@
 import { MdObject, Allergy } from '../../../../mdObject/src/classes/classes'
 import { AllergyClassification, AllergyCriticality } from '../../../../mdObject/src/enums/enums'
 import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
-import { ObjectState } from '../../../../mdObject/src/enums/objectState';
+import { ObjectState } from '../../../../mdObject/src/enums/enums';
 import { MdObjectServiceService } from '../md-object-service.service';
 
 @Pipe({ name: 'enumToArray'})
