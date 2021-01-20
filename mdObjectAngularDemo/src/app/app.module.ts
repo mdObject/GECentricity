22d6ed5da03d7d9a00669233e5f892a54001c366
elena <elena@DESKTOP-905UNLM>
2021-01-20 19:31:55 +0300

    Update pages.

--- mdObjectAngularDemo/src/app/app.module.ts
+++ mdObjectAngularDemo/src/app/app.module.ts
@@ -1,26 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { TitleCasePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { AllergyComponent, EnumToArrayPipe } from './allergy/allergy.component';
-import { MdObjectServiceService } from './md-object-service.service'
-
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { MatInputModule } from '@angular/material/input';
-import { TitleCasePipe } from '@angular/common';
-import { PatientHeaderComponent } from './patient-header/patient-header.component';
-import { PatientPersonalInformationComponent } from './components/patient-personal-information/patient-personal-information.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AllergyComponent, EnumToArrayPipe } from './allergy/allergy.component';
+import { AppComponent } from './app.component';
 import { PatientAddressComponent } from './components/patient-address/patient-address.component';
-import { SimulatorViewComponent } from './components/simulator-view/simulator-view.component';
 import { PatientContactsComponent } from './components/patient-contacts/patient-contacts.component';
+import { PatientPersonalInformationComponent } from './components/patient-personal-information/patient-personal-information.component';
 import { PatientPhoneComponent } from './components/patient-phone/patient-phone.component';
 import { PatientProblemsComponent } from './components/patient-problems/patient-problems.component';
+import { SimulatorViewComponent } from './components/simulator-view/simulator-view.component';
+import { MdObjectServiceService } from './md-object-service.service';
+import { PatientHeaderComponent } from './patient-header/patient-header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -34,7 +36,8 @@ import { PatientProblemsComponent } from './components/patient-problems/patient-
     PatientContactsComponent,
     PatientPhoneComponent,
     PatientProblemsComponent,
-    
+    SidenavComponent
+
   ],
   imports: [
     BrowserModule,
@@ -43,7 +46,8 @@ import { PatientProblemsComponent } from './components/patient-problems/patient-
     MatDatepickerModule,
     MatNativeDateModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSidenavModule
   ],
   providers: [TitleCasePipe, MdObjectServiceService],
   bootstrap: [AppComponent]
